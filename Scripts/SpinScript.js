0cc1baa5d069074fe519b21d8889c9a5e995d409
david <david@ongoingworlds.com>
2016-01-18 08:13:08 +0000

    Project start

---
+++ Scripts/SpinScript.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+public var spinX : float;
+public var spinY : float;
+public var spinZ : float;
+
+function Start () {
+	GetComponent.<Rigidbody>().AddTorque(spinX,spinY,spinZ);
+}
+
+function Update () {
+
+}
+
+//function FixedUpdate () {
+//	transform.Rotate((Vector3.up * rotationSpeed) * Time.deltaTime);
+//}
+
